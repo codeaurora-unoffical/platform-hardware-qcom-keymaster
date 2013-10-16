5123002ccb367da45b41d5968e1d490710ae98e1
Mona Hossain <mhossain@codeaurora.org>
2013-10-16 16:47:18 -0700

    keymaster: Fix make file and loading keymaster app
    Fix make file issues resulting in failing to build the right
    version of keystore.
    
    Change-Id: I7c4745aead0edd031a1875e700aad5ef0d91a189

--- Android.mk
+++ Android.mk
@@ -1,9 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
-
 include $(CLEAR_VARS)
 
-ifeq ($(strip $(TARGET_USES_HW_KEYMASTER)),true)
+ifneq ($(filter msm8960 msm8974 msm8226,$(TARGET_BOARD_PLATFORM)),)
 
 LOCAL_MODULE := keystore.qcom
 
@@ -11,23 +10,24 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
 LOCAL_SRC_FILES := keymaster_qcom.cpp
 
-LOCAL_C_INCLUDES := $(TARGET_OUT_HEADERS)/common/inc \
+LOCAL_C_INCLUDES :=  \
+                    $(TARGET_OUT_HEADERS)/common/inc \
                     $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
                     $(LOCAL_PATH)/../../../system/security/keystore \
                     $(LOCAL_PATH)/../../../external/openssl/include
 
 LOCAL_C_FLAGS = -fvisibility=hidden -Wall -Werror
 
-LOCAL_SHARED_LIBRARIES := libcrypto liblog libkeystore_client \
+LOCAL_SHARED_LIBRARIES := libcrypto liblog libkeystore_binder \
         libc \
         libcutils \
         libutils \
         libdl
 
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
-endif
+endif # TARGET_BOARD_PLATFORM
